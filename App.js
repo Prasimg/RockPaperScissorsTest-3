136e86444db2f8ceead83f2d710a64edca9aeeaa
Pratham <prasimg@gmail.com>
2022-10-16 00:52:42 +0530

    Initialized template for React app transfer

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+
+function App(){
+    return(
+        <div className="App">
+            {/*we need to render login, index and game.php*/ }
+        </div>
+    )
+}
